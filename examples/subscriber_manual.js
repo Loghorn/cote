37fcd808fbc347bc658fdf6bdf837ac669fe626d
Alessandro Vergani <alessandro.vergani@gmail.com>
2015-11-06 13:00:04 +0100

    Add re/discconnect to manual connections

--- examples/subscriber_manual.js
+++ examples/subscriber_manual.js
@@ -10,4 +10,8 @@ randomSubscriber.on('randomUpdate', function(req) {
     console.log('notified of ', req);
 });
 
-randomSubscriber.connectTo('127.0.0.1', 16275);
+randomSubscriber.connectTo('127.0.0.1', 16275, true);
+
+setTimeout(function() {
+    randomSubscriber.disconnectFrom('127.0.0.1', 16275);
+}, 10000);
